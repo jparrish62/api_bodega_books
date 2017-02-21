5d1c66bdb3c52dd1d50c00dc7c252ed349a9f942
Julian <parrishjulian62@gmail.com>
2017-02-21 10:29:18 -0500

    Added update

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ ApiBodegaBooks::Application.routes.draw do
   namespace :api, defaults: {format: :json},
                             constraints: {subdomain: 'api'}, path: '/' do
    scope module: :v1,
-         constraints: ApiConstraints.new(version: 1, default: true) do
-    resources :users,    :only => [:show,   :create, :update, :destroy]
-    resources :sessions, :only => [:create, :destroy]
-    resources :products, :only => [:show, :index]
-   end
+     constraints: ApiConstraints.new(version: 1, default: true) do
+       resources   :users,    :only => [:show,   :create, :update, :destroy] do
+         resources :products, :only => [:create, :update]
+       end
+       resources :sessions, :only => [:create, :destroy]
+       resources :products, :only => [:show, :index]
+     end
   end
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -29,4 +29,77 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    context 'when is successfully created' do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @product_attributes = FactoryGirl.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, params: {user_id: user.id, product: @product_attributes}
+      end
+
+      it "renders the json representation for the product record just created" do
+        products_response = json_response
+        expect(products_response[:title]).to eq @product_attributes[:title]
+      end
+      it {should respond_with 201}
+    end
+    context 'when is not created' do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @invalid_product_attributes = {title: "Smart TV", price: "Twelve Dollars"}
+        api_authorization_header user.auth_token
+        post :create, params: {user_id: user.id, product: @invalid_product_attributes}
+      end
+
+      it 'renders an error json' do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it {should respond_with 422}
+    end
+  end
+  describe "PUT/PATCH #update seccessfully" do
+    context "when is successfully updated" do
+      before(:each) do
+        @user    = FactoryGirl.create :user
+        @product = FactoryGirl.create :product, user: @user
+        api_authorization_header @user.auth_token
+        u = post :update, params: {user_id: @user.id, id: @product.id, product:{title: @product.title}}
+      end
+      it "renders the json representation for the updated user" do
+        product_response = json_response
+        expect(product_response[:title]).to eq @product.title
+      end
+      it {should respond_with 200}
+    end
+  end
+  describe "PUT/PATCH #update unsuccessfully" do
+    context "when is not updated" do
+      before(:each) do
+        @user    = FactoryGirl.create :user
+        @product = FactoryGirl.create :product, user: @user
+        api_authorization_header @user.auth_token
+      end
+      before(:each) do
+        patch :update, params: {user_id: @user.id, id: @product.id, product:{price: "two hundred"}}
+      end
+      it "renders the json errors on whye the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+      it "renders a json error" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+      it {should respond_with 422}
+    end
+  end
 end
